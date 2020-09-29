bdf67be8b24f97ed337dd181de3a4e15f058e1ad
thuynguyen8810 <70229553+thuynguyen8810@users.noreply.github.com>
2020-09-28 22:51:27 -0700

    Update Morty.hpp

--- src/Morty.hpp
+++ src/Morty.hpp
@@ -1,41 +1,41 @@
-/* 
- __  __            _               _                 
-|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \ 
+/*
+ __  __            _               _
+|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __
+| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \
 | |  | | (_) | |  | |_| |_| |  _  | | | | |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/ 
-                       |___/            |_|   |_|    
-*/ 
+|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/
+					   |___/            |_|   |_|
+*/
 
 #include <string> 
 #include <iostream>
+#pragma once
 
-
-/* Extra credit below:
-namespace interdimensional { 
+namespace interdimensional 
+{
 	// Declare enumeration to hold the dimensions each Morty is from
 	enum dimension {UNKNOWN = -1, C137, Z286};
-	
 	// Declare a structure to hold the arguments passed to the function
-	struct args {
+	struct args 
+	{
 		int start;
 		int stop;
 		int step;
 		dimension dim;
 	};
-	
+
 	// Declare a function to parse the args and return the above declared structure
-	args parseArgs(int c, char** v);
-	
+	args parseArgs (int c, char** v);
 }
-*/
 
-// =========================================================================== // 
-//          Declare Namespace C137 and enclosed functions here 
-// =========================================================================== // 
+namespace C137
+{
+	void Morty(int start, int stop, int step);
+	void Morty(int start, int stop);
+}
 
-
-
-// =========================================================================== // 
-//          Declare Namespace Z286 and enclosed functions here 
-// =========================================================================== // 
+namespace Z286
+{
+	void Morty(int start, int stop, int step);
+	void Morty(int start, int stop);
+}
