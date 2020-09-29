05b85233f217a9bae864d9cd2669fbff2d62aaa3
thuynguyen8810 <70229553+thuynguyen8810@users.noreply.github.com>
2020-09-28 22:46:57 -0700

    Update AwwGeezMan.cpp

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,48 +1,54 @@
 /*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
+ _____ ____ _____   ____ _____ ____
+| ____/ ___| ____| |___ \___  | ___|
+|  _|| |   |  _|     __) | / /|___ \
 | |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
+|_____\____|_____| |_____/_/  |____/
+ _   _                                         _      _
 | | | | ___  _ __ ___   _____      _____  _ __| | __ / |
 | |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
 |  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
 |_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
+
 Aww Geez Man: Interdimensional Mortys.
 
-One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
-exists a Morty who says everything backwards.
+One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there exists a Morty who says everything backwards.
+
+SUBMITTED BY: NGUYEN, THUY
 
 */
 
-// Include the Morty header file
+#include <iostream>
+#include <string>
+#include <string.h>
 #include "Morty.hpp"
 
 
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
-	if (ac != 4 and ac != 5) {
-		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
-			<< std::endl;
-		std::cout << "(1)\t./AwwGeezMan {start} {stop} {dimension}" << std::endl;
-		std::cout << "(2)\t./AwwGeezMan {start} {stop} {step} {dimension}" << std::endl;
-		
-		return -1;
-	}
-	
-	// Parse the command line arguments
-	
-		
-	// Depending on the dimension of the arguments, call the appropriate Morty
-	// function
-	
-	
-	return 0;
+int main(int ac, char** av)
+{
+    interdimensional::args saveArgs;
+ 
+    // Parse the command line arguments. The program is executed as
+    // AwwGeezMan {start} {stop} {dimension} 
+    // AwwGeezMan {start} {stop} {step} {dimension}
+
+    saveArgs = interdimensional::parseArgs(ac, av);
+
+    // Parse the command line arguments
+    // Depending on the dimension of the arguments, call the appropriate Morty
+    // function
+
+    switch (saveArgs.dim)
+    {
+        case 0: 
+            C137::Morty(saveArgs.start, saveArgs.stop, saveArgs.step);
+        case 1: 
+            Z286::Morty(saveArgs.start, saveArgs.stop, saveArgs.step);
+        case -1:
+        {
+            std::cout << "ERROR: unknown dimension" << std::endl;
+        }
+    }
+
+    return 0;
 }
